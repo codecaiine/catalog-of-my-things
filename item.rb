2423f443a2d44283583261d84cd00901f2fca245
codecaiine <akabrouyannickn@gmail.com>
2021-11-30 15:42:59 +0000

    Update main.rb

--- item.rb
+++ item.rb
@@ -1,24 +1,22 @@
-require "date"
+require 'date'
 
 class Item
   attr_accessor :archived, :publish_date
-  
-  
-    def initialize(genre, author, label, publish_date, archived = false)
-        @id = Random.rand(1..1000)
-        @author = author
-        @label = label
-        @publish_date = publish_date
-        @archived = archived
-    end
 
-   def can_be_archived?
+  def initialize(_genre, author, label, publish_date, archived = false)
+    @id = Random.rand(1..1000)
+    @author = author
+    @label = label
+    @publish_date = publish_date
+    @archived = archived
+  end
+
+  def can_be_archived?
     d = Date.today
     d > publish_date.next_year(10)
-   end
+  end
 
-   def move_to_archive
-      @archived = can_be_archived?
-   end
-
+  def move_to_archive
+    @archived = can_be_archived?
+  end
 end
--- main.rb
+++ main.rb
@@ -1,75 +1,8 @@
 require './app'
 
 class Main
-  def initialize
-    @main = 0
-    @book = []
-    @music_album =[]
-    @movie = []
-    @game = []
-
-    puts 'Welcome to Catalog My Things App!'
-    puts
-  end
-
-  def show_options
-    puts 'Please choose an option by entering a number:'
-    puts '1 -> List all books'
-    puts '2 -> List all music albums'
-    puts '3 -> List all movies'
-    puts '4 -> List of games'
-    puts '5 -> List all genres'
-    puts '6 -> List all labels'
-    puts '7 -> List all authors'
-    puts '8 -> List all sources'
-    puts '9 -> Add a book'
-    puts '10 -> Add a music album'
-    puts '11 -> Add a movie'
-    puts '12 -> Add a game'
-    puts '13 -> Exit'
-  end
-
-  def select_option
-    case @main
-    when 1
-      display_books
-    when 2
-      display_music_albums
-    when 3 
-      display_movies
-    when 4
-      display_games
-    when 5
-      display_genres
-    when 6
-      display_labels
-    when 7
-      display_authors
-    when 8
-      display_sources
-    when 9
-      user_book_input
-    when 10
-      user_music_input
-    when 11
-      user_movie_input
-    when 12
-      user_game_input
-    else 13
-      if @main != 13
-        puts 'Invalid input, please try again'
-        puts
-      end
-    end
-  end
-
-  def main
-  until @main == 13
-  show_options
-  @main = gets.chomp.to_i
-  end
-  puts 'Exiting session'
-  puts 'Thank you for using the Catalog Of My Things App'
+  def app; end
 end
 
-main
+main = Main.new
+main.app
