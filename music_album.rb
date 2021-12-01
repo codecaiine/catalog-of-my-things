855442282b4c8abadf326a4b1a155f6815543c76
Donard97 <donardgolaj@gmail.com>
2021-12-01 14:07:37 +0100

    Add properties to MusicAlbum class

--- music_album.rb
+++ music_album.rb
@@ -1,3 +1,7 @@
-class MusicAlbum
+require_relative './item.rb'
 
+class MusicAlbum < item
+  def intialize(publish_date, on_spotify: false, archived)
+    super(publish_date, archived)
+    @on_spotify = on_spotify
 end
