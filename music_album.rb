619373656873418c1aaec76bbe96fdfb5a54b12f
Donard97 <donardgolaj@gmail.com>
2021-12-01 14:08:11 +0100

    Add setter and reader to MusicAlbum class

--- music_album.rb
+++ music_album.rb
@@ -1,6 +1,9 @@
 require_relative './item.rb'
 
 class MusicAlbum < item
+  attr_accessor :publish_date, :on_spotify
+  attr_reader :archived
+
   def intialize(publish_date, on_spotify: false, archived)
     super(publish_date, archived)
     @on_spotify = on_spotify
