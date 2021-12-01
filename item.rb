efa35a5874d3ff1757e3b4ebb3e48f2fbf370d68
Kossi-stack <81444338+Kossi-stack@users.noreply.github.com>
2021-11-30 16:10:40 -0800

    implement mothod for item class

--- item.rb
+++ item.rb
@@ -1,8 +1,19 @@
+require "date"
+
 class Item
+  attr_accessor :publish_date
+  
+  
     def initialize(genre, author, label, publish_date, archived = false)
         @id = Random.rand(1..1000)
         @author = author
         @label = label
         @publish_date = publish_date
     end
+
+   def can_be_archived?
+    d = Date.today
+    d > publish_date.next_year(10)
+   end
+
 end
