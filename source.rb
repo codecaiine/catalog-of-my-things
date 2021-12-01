1cd4b7ed6fee6b1f1582d5ace4798e8147011317
Kossi-stack <81444338+Kossi-stack@users.noreply.github.com>
2021-12-01 15:35:16 -0800

    Initialize genre class

--- source.rb
+++ source.rb
@@ -1,5 +1,10 @@
-class Genre
-    attr_accessor :name
-    attr_reader :items
+class Source
+  attr_accessor :name
+  attr_reader :items
 
+  def initialize(name)
+    @id = Random.rand(1..1000)
+    @name = name
+    @items = []
+  end
 end
