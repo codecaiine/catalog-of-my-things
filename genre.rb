2ffee75a366280680069ee95a7a4e3cbb4ef57fa
Donard97 <donardgolaj@gmail.com>
2021-12-01 14:18:49 +0100

    Fix linters errors

--- genre.rb
+++ genre.rb
@@ -3,8 +3,8 @@ class Genre
   attr_reader :items
 
   def initialize(name)
-   @id = Random.rand(1..1000)
-   @name = name
-   @items = []
-  end    
+    @id = Random.rand(1..1000)
+    @name = name
+    @items = []
+  end
 end
