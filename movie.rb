de8f7e391ab71b51d19cf410b7b576cb76fdb786
codecaiine <akabrouyannickn@gmail.com>
2021-12-01 21:42:08 +0000

    Fix linters errors

--- movie.rb
+++ movie.rb
@@ -1,15 +1,14 @@
 require_relative 'item'
 
 class Movie
-    attr_reader :publish_date, :silent
-    
-    def initialize(publish_date, silent: false)
-        super(publish_date)
-        @silent = silent
-    end
-    
-    def can_be_archived?
-        super || @silent
-    end
-    
+  attr_reader :publish_date, :silent
+
+  def initialize(publish_date, silent: false)
+    super(publish_date)
+    @silent = silent
+  end
+
+  def can_be_archived?
+    super || @silent
+  end
 end
--- source.rb
+++ source.rb
@@ -1,18 +1,18 @@
 class Source
-    attr_reader :items, :name
+  attr_reader :items, :name
 
-    def initialize(name, id = Random.rand(1..100))
-        @id = id
-        @name = name
-        @items = []
-    end
+  def initialize(name, id = Random.rand(1..100))
+    @id = id
+    @name = name
+    @items = []
+  end
 
-    def add_item(item)
-        if item.instance_of?(Item)
-          @items << item
-          item.add_source(self)
-        else
-          'Please insert a item here'
-        end
+  def add_item(item)
+    if item.instance_of?(Item)
+      @items << item
+      item.add_source(self)
+    else
+      'Please insert a item here'
     end
+  end
 end
