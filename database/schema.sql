3603983b1a304254cb59df3bd92ba5a9ee6917bd
Donard97 <donardgolaj@gmail.com>
2021-12-04 00:27:31 +0100

    Create table of games on database

--- database/schema.sql
+++ database/schema.sql
@@ -72,4 +72,11 @@ CREATE TABLE genres(
   PRIMARY KEY(id)
 );
 
+CREATE TABLE games(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  multiplayer BOOLEAN,
+  last_played_at DATE,
+  FOREIGN KEY(id) REFERENCES item(id)
+);
+
 
