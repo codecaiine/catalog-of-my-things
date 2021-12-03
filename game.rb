7da75e340c1a404e341edadf795e634983ce07da
ahzia <ah.ziayosfi@gmail.com>
2021-12-03 14:13:10 +0430

    create game class

---
+++ game.rb
@@ -0,0 +1,16 @@
+require './item'
+
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+
+  def initialize(publish_date, multiplayer, last_played_at)
+    super(publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = Date.parse(last_played_at)
+  end
+
+  def can_be_archived?()
+    today_date = Date.today
+    super() && @last_played_at.next_year(2) < today_date
+  end
+end
