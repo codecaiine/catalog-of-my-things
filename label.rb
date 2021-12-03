1343dfacaaf7e837b3547a76e1c59e86221a3dc5
codecaiine <akabrouyannickn@gmail.com>
2021-12-03 16:20:34 +0000

    Add inherite Label class from item class

--- label.rb
+++ label.rb
@@ -1,3 +1,11 @@
+require_relative 'item'
+
 class Label
-    
+  attr_accessor :title, :color, :items
+
+  def initialize(title, color)
+    @title = title
+    @color = color
+    @items = []
+  end
 end
