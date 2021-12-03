e165d344ee72f0289d1e5a87c294c4e08ec9c640
codecaiine <akabrouyannickn@gmail.com>
2021-12-03 17:17:23 +0000

    Update test error

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,24 +1,24 @@
 require_relative '../book'
 
 describe Book do
-    publisher = 'Iron Man'
-    cover_state = 'good'
-    publish_date = 10
-    let(:book) { Book.new(publisher, cover_state, publish_date) }
-  
-    it 'instance of Book' do
-      expect(book).to be_an_instance_of(Book)
-    end
-  
-    it 'should return the publisher of the book object' do
-      expect(book.publisher).to eq(publisher)
-    end
-  
-    it 'should return the cover_state of the book object' do
-      expect(book.cover_state).to eq(cover_state)
-    end
-  
-    it 'Test for can_be_archived? method' do
-      expect(book.can_be_archived?).to eq(true)
-    end
+  publisher = 'Iron Man'
+  cover_state = 'good'
+  publish_date = 10
+  let(:book) { Book.new(publisher, cover_state, publish_date) }
+
+  it 'instance of Book' do
+    expect(book).to be_an_instance_of(Book)
   end
+
+  it 'should return the publisher of the book object' do
+    expect(book.publisher).to eq(publisher)
+  end
+
+  it 'should return the cover_state of the book object' do
+    expect(book.cover_state).to eq(cover_state)
+  end
+
+  it 'Test for can_be_archived? method' do
+    expect(book.can_be_archived?).to eq(true)
+  end
+end
