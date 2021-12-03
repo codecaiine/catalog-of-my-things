a4df8c92d497591da17c61d8c2a6c230be672e14
codecaiine <akabrouyannickn@gmail.com>
2021-12-03 23:14:36 +0000

    Create book table

--- database/schema.sql
+++ database/schema.sql
@@ -31,3 +31,31 @@ CREATE TABLE item(
       REFERENCES label(id)
       ON DELETE CASCADE
 );
+
+CREATE TABLE book(
+  id SERIAL PRIMARY KEY,
+  publisher VARCHAR(80),
+  cover_state VARCHAR(20),
+  publish_date date,
+  archived boolean,
+  genre_id INT,
+  source_id INT,
+  author_id INT,
+  label_id INT,
+  CONSTRAINT genre_id
+    FOREIGN KEY(genre_id)
+      REFERENCES genre(id)
+      ON DELETE CASCADE,
+  CONSTRAINT source_id
+    FOREIGN KEY(source_id)
+      REFERENCES source(id)
+      ON DELETE CASCADE,
+  CONSTRAINT author_id
+    FOREIGN KEY(author_id)
+      REFERENCES author(id)
+      ON DELETE CASCADE,
+  CONSTRAINT label_id
+    FOREIGN KEY(label_id)
+      REFERENCES label(id)
+      ON DELETE CASCADE
+);
