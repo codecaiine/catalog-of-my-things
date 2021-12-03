c4b94fb4647c63e990f1017c53b00db8eb9a442d
codecaiine <akabrouyannickn@gmail.com>
2021-12-03 23:13:35 +0000

    Create item table

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,29 @@ CREATE TABLE label(
   title VARCHAR(30),
   color VARCHAR(20),
 );
+
+CREATE TABLE item(
+  id SERIAL PRIMARY KEY,
+  publish_date date,
+  archived boolean,
+  genre_id INT,
+  source_id INT,
+  author_id INT,
+  label_id INT,
+  CONSTRAINT genre_id
+    FOREIGN KEY(genre_id)
+      REFERENCES genre(id)
+      ON DELETE CASCADE,
+  CONSTRAINT source_id
+    FOREIGN KEY(source_id)
+      REFERENCES source(id)
+      ON DELETE CASCADE,
+  CONSTRAINT author_id
+    FOREIGN KEY(author_id)
+      REFERENCES author(id)
+      ON DELETE CASCADE,
+  CONSTRAINT label_id
+    FOREIGN KEY(label_id)
+      REFERENCES label(id)
+      ON DELETE CASCADE
+);
