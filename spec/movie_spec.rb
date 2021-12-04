35af49856823bf56eb211a50f8d9ac4d89828b5f
Kossi-stack <81444338+Kossi-stack@users.noreply.github.com>
2021-12-03 18:19:16 -0800

    Fix linters errors

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -7,15 +7,15 @@ describe Movie do
     end
 
     it 'The new_movie object is an instance of Movie' do
-        expect(@new_movie).to be_instance_of Movie
+      expect(@new_movie).to be_instance_of Movie
     end
 
     it 'The new_movie object is an instance of (Inherted from) Item' do
-        expect(@new_movie).to be_kind_of Item
+      expect(@new_movie).to be_kind_of Item
     end
 
     it 'Archived to be false as last_played_at is not from more than tow years ago' do
-        expect(@new_movie.move_to_archive).to be true
-      end
+      expect(@new_movie.move_to_archive).to be true
     end
+  end
 end
--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -7,22 +7,22 @@ describe Source do
       @new_source = Source.new('Suit', 'Avocats')
       @new_item = Item.new('2020-01-10')
     end
-    
+
     it 'new_source is an instance of source' do
-        expect(@new_source).to be_instance_of(Source)
+      expect(@new_source).to be_instance_of(Source)
     end
 
     it 'items is empty' do
-        expect(@new_source.show_items).to be_empty
+      expect(@new_source.show_items).to be_empty
     end
 
     it 'Should add item to Items array' do
-        @new_source.add_item(@new_item)
-        expect(@new_source.show_items).to match_array([@new_item])
+      @new_source.add_item(@new_item)
+      expect(@new_source.show_items).to match_array([@new_item])
     end
-  
+
     it 'The source of new_item should be new_source' do
-        expect(@new_item.source).to eql @new_source
+      expect(@new_item.source).to eql @new_source
     end
   end
 end
