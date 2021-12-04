2ff99b3700a20d0ac78f33f7b46f8b24ef8dfa1e
Kossi-stack <81444338+Kossi-stack@users.noreply.github.com>
2021-12-03 17:39:30 -0800

    First instance of test for source

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -7,6 +7,9 @@ describe Author do
       @new_author = Author.new('Ahmad', 'Mahmmod')
       @new_item = Item.new('2020-10-10')
     end
-
     
+    it 'new_author is an instance of Author' do
+        expect(@new_author).to be_instance_of(Author)
+    end
+
 end
