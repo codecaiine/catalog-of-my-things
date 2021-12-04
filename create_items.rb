f2cc3740dcae5ebc85cc39120aa02afbe95765d6
ahzia <ah.ziayosfi@gmail.com>
2021-12-04 21:35:02 +0430

    add the missing functions in create item

--- create_items.rb
+++ create_items.rb
@@ -10,21 +10,13 @@ module CreateItems
     puts 'Game Created'
   end
 
-  def create_author
-    print 'first_name:'
-    first_name = gets.chomp
-    print 'last_name'
-    last_name = gets.chomp
-    @authors << Author.new(first_name, last_name)
-  end
-
   def create_music_album
     puts
 
     print 'Published date: '
     publish_date = gets.chomp
 
-    print 'On spotify? [Y/N]: '
+    print 'On spotify? [y/n]: '
     on_spotify = gets.chomp != 'n'
 
     music = MusicAlbum.new(publish_date, on_spotify)
@@ -33,13 +25,6 @@ module CreateItems
     puts
     puts 'Music album is created succussfully!'
     puts
-  end
-
-  def create_genre
-    print 'Genre name: '
-    genre = gets.chomp
-    @genres << Genre.new(genre)
-    puts 'Genre has been added successfully'
   end
 
   def create_book
@@ -54,6 +39,7 @@ module CreateItems
 
     @books << Book.new(publish_date, publisher, cover_state)
     puts 'Book has been added successfully'
+    create_associations(@books[-1])
   end
 
   def create_movie
@@ -65,5 +51,109 @@ module CreateItems
 
     @movies << Movie.new(publish_date, silet)
     puts 'Movie has been added successfully'
+  end
+
+  def create_genre(item = nil)
+    print 'Genre name: '
+    genre = gets.chomp
+    puts 'Genre has been added successfully'
+    @genres << Genre.new(genre) if item.nil?
+    new_genre = Genre.new(genre)
+    new_genre.add_item(item)
+    @genres << new_genre
+  end
+
+  def create_author(item = nil)
+    print 'first_name:'
+    first_name = gets.chomp
+    print 'last_name'
+    last_name = gets.chomp
+    @authors << Author.new(first_name, last_name) if item.nil?
+    new_author = Author.new(first_name, last_name)
+    new_author.add_item(item)
+    @authors << new_author
+  end
+
+  def create_label(item = nil)
+    print 'title:'
+    title = gets.chomp
+    print 'color:'
+    color = gets.chomp
+    @labels << Label.new(title, color) if item.nil?
+    new_label = Label.new(title, color)
+    new_label.add_item(item)
+    @labels << new_label
+  end
+
+  def create_source(item = nil)
+    print 'name:'
+    name = gets.chomp
+    @sources << Source.new(name) if item.nil?
+    new_source = Source.new(name)
+    new_source.add_item(item)
+    @sources << new_source
+  end
+
+  def create_associations(item)
+    puts 'Do you want to add any association [y/n]:'
+    input = gets.chomp != 'n'
+    if input
+      puts 'What association do you want to add to the created item:'
+      puts '[1]: Genre, [2]:Source, [3]:Label, [4]:Author, [5]:Back'
+      association = gets.chomp.to_i
+      case association
+      when 1
+        puts 'Do you want to create a new Genre[y] or use an exsisting one[n] [y/n]:'
+        input = gets.chomp != 'n'
+        if input
+          create_genre(item)
+        else
+          puts 'Select a genre by index:'
+          display_genres
+          genre_index = gets.chomp.to_i
+          @genres[genre_index].add_item(item)
+          puts 'item has been added to genre'
+        end
+      when 2
+        puts 'Do you want to create a new Source[y] or use an exsisting one[n] [y/n]:'
+        input = gets.chomp != 'n'
+        if input
+          create_source(item)
+        else
+          puts 'Select a Source by index:'
+          display_sources
+          source_index = gets.chomp.to_i
+          @sources[source_index].add_item(item)
+          puts 'item has been added to source'
+        end
+      when 3
+        puts 'Do you want to create a new Label[y] or use an exsisting one[n] [y/n]:'
+        input = gets.chomp != 'n'
+        if input
+          create_label(item)
+        else
+          puts 'Select a Label by index:'
+          display_labels
+          label_index = gets.chomp.to_i
+          @labels[label_index].add_item(item)
+          puts 'item has been added to label'
+        end
+      when 4
+        puts 'Do you want to create a new Author[y] or use an exsisting one[n] [y/n]:'
+        input = gets.chomp != 'n'
+        if input
+          create_author(item)
+        else
+          puts 'Select a Label by index:'
+          display_authors
+          author_index = gets.chomp.to_i
+          @authors[author_index].add_item(item)
+          puts 'item has been added to Author'
+        end
+      else
+        puts
+      end
+      create_associations(item)
+    end
   end
 end
--- display_items.rb
+++ display_items.rb
@@ -10,6 +10,7 @@ module DisplayItems
         print "publish_date: #{game.publish_date} "
         print "multiplayer: #{game.multiplayer} "
         puts "last_played_at: #{game.last_played_at} "
+        display_more_details(game)
         index += 1
       end
     end
@@ -23,6 +24,7 @@ module DisplayItems
       index = 0
       @music_albums.each do |album|
         puts "#{index} - Album: Publish date: #{album.publish_date}, On Spotify: #{album.on_spotify}"
+        display_more_details(album)
         index += 1
       end
     end
@@ -38,6 +40,7 @@ module DisplayItems
         print "#{index} - movie: "
         print "publish_date: #{movie.publish_date} "
         puts "multiplayer: #{movie.silet} "
+        display_more_details(movie)
         index += 1
       end
     end
@@ -54,8 +57,16 @@ module DisplayItems
         print "publish_date: #{book.publish_date} "
         print "publisher: #{book.publisher} "
         puts "cover_state: #{book.cover_state} "
+        display_more_details(book)
         index += 1
       end
     end
+  end
+
+  def display_more_details(item)
+    puts "Genre: #{item.genre.name}" if item.genre
+    puts "Author: first name: #{item.author.first_name} last name: #{item.author.last_name}" if item.author
+    puts "Label : title: #{item.label.title} color: #{item.label.color}" if item.label
+    puts "Source: #{item.name.source}" if item.source
   end
 end
