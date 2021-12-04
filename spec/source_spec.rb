a5114cc18cce591630206264aab179b322624fad
Kossi-stack <81444338+Kossi-stack@users.noreply.github.com>
2021-12-03 18:01:37 -0800

    test for source

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -1,15 +1,26 @@
 require_relative '../item'
-require_relative '../author'
+require_relative '../source'
 
-describe Author do
-  context 'Testing Author Class' do
+describe Source do
+  context 'Testing Source Class' do
     before(:all) do
-      @new_author = Author.new('Ahmad', 'Mahmmod')
-      @new_item = Item.new('2020-10-10')
+      @new_source = Source.new('Suit', 'Avocats')
+      @new_item = Item.new('2020-01-10')
     end
     
-    it 'new_author is an instance of Author' do
-        expect(@new_author).to be_instance_of(Author)
+    it 'new_source is an instance of source' do
+        expect(@new_source).to be_instance_of(Source)
     end
 
+    it 'items is empty' do
+        expect(@new_source.show_items).to be_empty
+    end
+
+    it 'Should add item to Items array' do
+        @new_source.add_item(@new_item)
+        expect(@new_source.show_items).to match_array([@new_item])
+    end
+  
+   
+  end
 end
